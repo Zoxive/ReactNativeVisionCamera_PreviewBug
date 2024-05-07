d0906fc577df5187ad4615b112b5804d7a354b0c
Kyle Wascher <kyle@zoxive.com>
2024-05-07 10:13:37 -0400

    starting setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<StatusBar style="auto" />
+			<View style={styles.container}>
+				<ViewPort />
+        <CameraControls />
+			</View>
+		</>
+	);
+}
+
+function ViewPort() {
+	return <View style={styles.viewPort} />;
+}
+
+function CameraControls() {
+  return <View style={styles.cameraControls} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+    flexDirection: 'column',
+	},
+  viewPort: {
+    aspectRatio: 9 / 16,
+    backgroundColor: 'red',
+  },
+  cameraControls: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
   },
 });
